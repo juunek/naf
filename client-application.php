654f4a36e22f9caa2f4fb37eb2145bca67cc8ba0
kathrynkerr <57072945+kathrynkerr@users.noreply.github.com>
2021-04-15 22:11:20 -0500

    Update client-application.php

--- client-application.php
+++ client-application.php
@@ -75,7 +75,58 @@
 </div>
 
 <div>
-  <form>
+  <form action="" method="post">
+    <h3>Patient/Client Info</h3>
+    <div class="row">
+      <div class="d-flex flex-column col-6">
+        <label>First Name</label>
+        <input type="text" id="client_firstname">
+      </div>
+
+      <div class="d-flex flex-column col-6">
+        <label>Last Name</label>
+        <input type="text" id="client_lastname">
+      </div>
+
+      <div class="d-flex flex-column col-6">
+        <label>Birthdate (mm/dd/yyyy)</label>
+        <input type="date" id="client_birthday">
+      </div>
+
+      <div class="d-flex flex-column col-12">
+        <label>Street Address</label>
+        <input type="text" id="client_address">
+      </div>
+
+      <div class="d-flex flex-column col-6">
+        <label>City</label>
+        <input type="text" id="client_city">
+      </div>
+
+      <div class="d-flex flex-column col-3">
+        <label>State</label>
+        <input type="text" id="client_state">
+      </div>
+
+      <div class="d-flex flex-column col-3">
+        <label>Zip Code</label>
+        <input type="text" id="client_zip">
+      </div>
+      <div class="d-flex flex-column col-6">
+        <label>Patient has children at home</label>
+        <div class="d-flex flex-row justify-content-between">
+          <div>
+            <input type="radio" name="client_childrenHome" value="client_childrenHome_yes" id="client_childrenHome_yes">
+            <label for="client_childrenHome_yes">Yes</label>
+          </div>
+          <div>
+            <input type="radio" name="client_childrenHome" value="client_childrenHome_no" id="client_childrenHome_no">
+            <label for="client_childrenHome_no">No</label>
+        </div>
+        </div>
+      </div>
+    </div>
+
   </form>
 </div>
 <?php
