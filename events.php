d762efa2c064fbb05b26b8b447beaa7999828b5a
kathrynkerr <57072945+kathrynkerr@users.noreply.github.com>
2021-04-16 18:18:54 -0500

    Update events.php

--- events.php
+++ events.php
@@ -2,6 +2,26 @@
   include("shared.php");
   print($htmlNav);
 ?>
+<div class="container">
+  <h1 class="my-5 text-center">Events</h1>
+
+  <!--Local Resources -->
+
+  <h2 class="resources-heading">Upcoming Events</h2>
+  <br>
+  <div>
+    <div class="col-md-10 mx-auto row">
+
+        <div class="col-md-6">
+          <img src="img/events-cinco-de-mayo.jpg">
+        </div>
+        <div class="col-md-6">
+          <h3>Cinco de Mayo</h3>
+        </div>
+
+    </div>
+  </div>
+</div>
 <?php
   print($htmlFooter);
 ?>
