a854d077801bac4fc6bed048e6a575fe85dc6977
kathrynkerr <57072945+kathrynkerr@users.noreply.github.com>
2021-04-15 21:04:57 -0500

    client application

--- client-application.php
+++ client-application.php
@@ -2,6 +2,82 @@
   include("shared.php");
   print($htmlNav);
 ?>
+<div class="container">
+  <h1 class="my-5 text-center">Request Assistance</h1>
+
+  <!--Local Resources -->
+
+  <h2 class="resources-heading">Do you need assistance?</h2>
+  <br>
+  <div class="test">
+    <p>Are you living with a spinal cord injury, spinal cord disability, or congenital disorder (birth defect)? NAF's mission is to help with mobility equipment, home modifications, vehicle modifications and technology to ensure maximum independence and self-sufficiency for our clients.</p>
+    <p>We assist people in the following Texas counties:</p>
+    <div class="row pb-4">
+      <div class="col-md-3 col-sm-3 col-6" id="sm-client-app-counties-first">
+        <p>
+          Austin<br>
+          Bosque<br>
+          Brazoria<br>
+          Chambers<br>
+          Collin<br>
+          Cooke<br>
+          Dallas<br>
+          Delta<br>
+          Denton
+        </p>
+      </div>
+      <div class="col-md-3 col-sm-3 col-6" id="sm-client-app-counties-third">
+        <p>
+          Ellis<br>
+          Erath<br>
+          Fannin<br>
+          Fort Bend<br>
+          Galveston<br>
+          Grayson<br>
+          Harris<br>
+          Henderson<br>
+          Hill
+        </p>
+      </div>
+      <div class="col-md-3 col-sm-3 col-6" id="sm-client-app-counties-second">
+        <p>
+          Hood<br>
+          Hunt<br>
+          Jack<br>
+          Johnson<br>
+          Kaufman<br>
+          Liberty<br>
+          Montague<br>
+          Montgomery<br>
+          Navarro
+        </p>
+      </div>
+      <div class="col-md-3 col-sm-3 col-6" id="sm-client-app-counties-fourth">
+        <p>
+          Palo Pinto<br>
+          Parker<br>
+          Rains<br>
+          Rockwall<br>
+          Somervell<br>
+          Tarrant<br>
+          Van Zandt<br>
+          Waller<br>
+          Wise
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <h2 class="resources-heading">Client Application</h2>
+  <br>
+  <div class="test">
+    <p>Thank you for your interest in NAF. To request assistance, please complete the application below. Applications must be completed by a physician, therapist, or hospital case manager, and clients must not have alternative means to receive assistance. We will contact you after the application is received.</p>
+</div>
+
+<div>
+  <form>
+  </form>
+</div>
 <?php
   print($htmlFooter);
 ?>
