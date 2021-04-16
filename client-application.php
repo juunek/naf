cd356fbcead72be11db3a86ac1d13c9e7802e863
kathrynkerr <57072945+kathrynkerr@users.noreply.github.com>
2021-04-15 23:26:42 -0500

    Finished HTML for Client App

--- client-application.php
+++ client-application.php
@@ -114,18 +114,198 @@
       </div>
       <div class="d-flex flex-column col-6">
         <label>Patient has children at home</label>
-        <div class="d-flex flex-row justify-content-between">
-          <div>
-            <input type="radio" name="client_childrenHome" value="client_childrenHome_yes" id="client_childrenHome_yes">
-            <label for="client_childrenHome_yes">Yes</label>
+          <div class="d-flex flex-row">
+            <div class="pe-5">
+              <input type="radio" name="client_childrenHome" value="client_childrenHome_yes" id="client_childrenHome_yes">
+              <label for="client_childrenHome_yes">Yes</label>
+            </div>
+            <div>
+              <input type="radio" name="client_childrenHome" value="client_childrenHome_no" id="client_childrenHome_no">
+              <label for="client_childrenHome_no">No</label>
           </div>
-          <div>
-            <input type="radio" name="client_childrenHome" value="client_childrenHome_no" id="client_childrenHome_no">
-            <label for="client_childrenHome_no">No</label>
+        </div>
+      </div>
+
+      <div class="d-flex flex-column col-6">
+        <label>Ages of Children</label>
+        <input type="text" id="client_childrenAge">
+      </div>
+
+      <div class="d-flex flex-column col-12">
+        <label>Describe spinal cord injury (level and cause, i.e., MVA, Fall, etc.). If not a traumatic injury, please explain</label>
+        <textarea name="client_explaination" id="client_explaination"></textarea>
+      </div>
+
+      <div class="d-flex flex-column col-12">
+        <label>Type of assistance requested</label>
+          <div class="d-flex flex-row">
+            <div class="col-3">
+              <input type="checkbox" name="client_assistType" value="client_assistType_home" id="client_assistType_home">
+              <label for="client_assistType_home">Home Modifications</label>
+            </div>
+            <div class="col-3">
+              <input type="checkbox" name="client_assistType" value="client_assistType_mobility" id="client_assistType_mobility">
+              <label for="client_assistType_mobility">Mobility Equipment</label>
+          </div>
+          <div class="col-3">
+            <input type="checkbox" name="client_assistType" value="client_assistType_catheters" id="client_assistType_catheters">
+            <label for="client_assistType_catheters">Catheters</label>
+          </div>
+          <div class="col-3">
+            <input type="checkbox" name="client_assistType" value="client_assistType_other" id="client_assistType_other">
+            <label for="client_assistType_other">Other</label>
         </div>
         </div>
       </div>
+
+      <div class="d-flex flex-column col-12">
+        <label>Describe patient's needs in order of importance</label>
+        <textarea name="client_needs" id="client_needs"></textarea>
+      </div>
+
+      <div class="d-flex flex-column col-6">
+        <label>Patient discharge date (mm/dd/yyyy)</label>
+        <input type="date" id="client_discharge">
+      </div>
+
+      <div class="d-flex flex-column col-6">
+        <label>Will patient be using catheters</label>
+            <div class="d-flex flex-row">
+              <div class="pe-5">
+                <input type="radio" name="client_catheterUse" value="client_catheterUse_yes" id="client_catheterUse_yes">
+                <label for="client_catheterUse_yes">Yes</label>
+              </div>
+            <div>
+              <input type="radio" name="client_catheterUse" value="client_catheterUse_no" id="client_catheterUse_no">
+              <label for="client_catheterUse_no">No</label>
+          </div>
+        </div>
+      </div>
+
+      <div class="d-flex flex-column col-4">
+        <label>Is patient insured?</label>
+          <div class="d-flex flex-row">
+            <div class="pe-5">
+              <input type="radio" name="client_insured" value="client_insured_yes" id="client_insured_yes">
+              <label for="client_insured_yes">Yes</label>
+            </div>
+            <div>
+              <input type="radio" name="client_insured" value="client_insured_no" id="client_insured_no">
+              <label for="client_insured_no">No</label>
+          </div>
+        </div>
+      </div>
+
+      <div class="d-flex flex-column col-4">
+        <label>Does patient have Medicare/Medicaid</label>
+          <div class="d-flex flex-row">
+              <div class="pe-5">
+                <input type="radio" name="client_medicare" value="client_medicare_yes" id="client_medicare_yes">
+                <label for="client_medicare_yes">Yes</label>
+              </div>
+              <div class="pe-5">
+                <input type="radio" name="client_medicare" value="client_medicare_no" id="client_medicare_no">
+                <label for="client_medicare_no">No</label>
+            </div>
+            <div>
+              <input type="radio" name="client_medicare" value="client_medicare_unsure" id="client_medicare_unsure">
+              <label for="client_medicare_unsure">Awaiting Approval</label>
+          </div>
+        </div>
+      </div>
+
+      <div class="d-flex flex-column col-4">
+        <label>Is patient a veteran?</label>
+          <div class="d-flex flex-row">
+            <div class="pe-5">
+              <input type="radio" name="client_veteran" value="client_veteran_yes" id="client_veteran_yes">
+              <label for="client_veteran_yes">Yes</label>
+            </div>
+            <div>
+              <input type="radio" name="client_veteran" value="client_veteran_no" id="client_veteran_no">
+              <label for="client_veteran_no">No</label>
+          </div>
+        </div>
+      </div>
+
+      <div class="d-flex flex-column col-12">
+        <label>Describe other program eligibility</label>
+        <textarea name="client_eligibility" id="client_eligibility"></textarea>
+      </div>
     </div>
+
+    <h3>Social Worker Contact Info</h3>
+    <div class="row">
+      <div class="d-flex flex-column col-6">
+        <label>First Name</label>
+        <input type="text" id="client_social_firstname">
+      </div>
+
+      <div class="d-flex flex-column col-6">
+        <label>Last Name</label>
+        <input type="text" id="client_social_lastname">
+      </div>
+
+      <div class="d-flex flex-column col-6">
+        <label>Email Address</label>
+        <input type="email" id="client_social_email">
+      </div>
+
+      <div class="d-flex flex-column col-6">
+        <label>Phone Number</label>
+        <input type="tel" id="client_social_phone">
+      </div>
+    </div>
+
+    <h3>Person Submitting Contact Info</h3>
+    <div class="row">
+      <div class="d-flex flex-column col-6">
+        <label>First Name</label>
+        <input type="text" id="client_contact_firstname">
+      </div>
+
+      <div class="d-flex flex-column col-6">
+        <label>Last Name</label>
+        <input type="text" id="client_contact_lastname">
+      </div>
+
+      <div class="d-flex flex-column col-6">
+        <label>Email Address</label>
+        <input type="email" id="client_contact_email">
+      </div>
+
+      <div class="d-flex flex-column col-6">
+        <label>Phone Number</label>
+        <input type="tel" id="client_contact_phone">
+      </div>
+    </div>
+
+    <h3>Primary Contact Info</h3>
+    <div class="row">
+      <div class="d-flex flex-column col-6">
+        <label>First Name</label>
+        <input type="text" id="client_primary_firstname">
+      </div>
+
+      <div class="d-flex flex-column col-6">
+        <label>Last Name</label>
+        <input type="text" id="client_primary_lastname">
+      </div>
+
+      <div class="d-flex flex-column col-6">
+        <label>Email Address</label>
+        <input type="email" id="client_primary_email">
+      </div>
+
+      <div class="d-flex flex-column col-6">
+        <label>Phone Number</label>
+        <input type="tel" id="client_primary_phone">
+      </div>
+    </div>
+
+    <div class="col-12">
+		<input type="submit" value="Submit Query" name="client_submit" id="client_submit">
+	</div>
 
   </form>
 </div>
