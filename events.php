6be5ce6986e6b288f720c0c7b112ef3fab0ef037
kathrynkerr <57072945+kathrynkerr@users.noreply.github.com>
2021-04-17 00:10:12 -0500

    events

--- events.php
+++ events.php
@@ -10,13 +10,34 @@
   <h2 class="resources-heading">Upcoming Events</h2>
   <br>
   <div>
-    <div class="col-md-10 mx-auto row">
+    <div class="col-md-10 mx-auto row py-4">
 
         <div class="col-md-6">
-          <img src="img/events-cinco-de-mayo.jpg">
+          <img class="cover" src="img/events-cinco-de-mayo.jpg">
         </div>
+        <div class="d-flex align-items-start flex-column col-md-6">
+          <h3>Cinco de Mayo Run, Walk, &amp Roll</h3>
+          <p>Saturday, May 1, 2021</p>
+          <p>6:15 AM–10:15 AM</p>
+          <p>McKinney, Texas</p>
+          <p>Run, Walk or Roll with us to help raise money for people in our community living with spinal cord injuries and disabilities.</p>
+          <button class="mt-auto">View Event</button>
+        </div>
+
+    </div>
+
+    <div class="col-md-10 mx-auto row py-4">
+
         <div class="col-md-6">
-          <h3>Cinco de Mayo</h3>
+          <img class="cover" src="img/events-topgolf-charity-event.jpg">
+        </div>
+        <div class="d-flex align-items-start flex-column col-md-6">
+          <h3>Cinco de Mayo Run, Walk, &amp Roll</h3>
+          <p>Saturday, May 1, 2021</p>
+          <p>6:15 AM–10:15 AM</p>
+          <p>McKinney, Texas</p>
+          <p>Run, Walk or Roll with us to help raise money for people in our community living with spinal cord injuries and disabilities.</p>
+          <button class="mt-auto">View Event</button>
         </div>
 
     </div>
