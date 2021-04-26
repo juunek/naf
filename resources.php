fddcfa3ab7f365f08431639dde42a75c12b5e4bf
tinnguyen-uta <57383385+tinnguyen-uta@users.noreply.github.com>
2021-04-25 23:23:59 -0500

    Updated resources.php

--- resources.php
+++ resources.php
@@ -1,109 +1,124 @@
 <?php
-  include("shared.php");
-  print($htmlNav);
+
+include("dbconn.inc.php"); // database connection 
+include("shared.php"); // stored shared contents, such as HTML header and page title, page footer, etc. in variables
+
+// make database connection
+$conn = dbConnect();
+  
 ?>
+
+<?php 
+	print($htmlNav);
+?>
+
+
+
+<?php 
+
+	
+	$sql = "SELECT Resources.ResID, Resources.Img, Resources.Title, Resources.Lead, Resources.Description, Resources.Link, Resources.RID FROM Resources ORDER BY Resources.RID";
+
+	$stmt = $conn->stmt_init();
+		
+		if ($stmt->prepare($sql)) {
+			
+			/* execute statement */
+			$stmt->execute();
+
+			$stmt->bind_result($ResID, $Img, $Title, $Lead, $Description, $Link, $RID);
+
+			$output = "";
+
+			$arrImg = array();
+			$arrTitle = array();
+			$arrLead = array();
+			$arrDescription = array();
+			$arrLink = array();
+			$arrRID = array();
+
+			while ($stmt->fetch()) {
+
+				
+				array_push($arrImg, $Img);
+				array_push($arrTitle, $Title);
+				array_push($arrLead, $Lead);
+				array_push($arrDescription, $Description);
+				array_push($arrLink, $Link);
+				array_push($arrRID, $RID);
+
+			}
+
+
+
+			$arrRName = ["Local Resources", "Media Resources", "National Resources"];
+
+			
+			for ($i=0; $i < 3 ; $i++) { 
+
+
+				$output = $output."<h2 class='resources-heading bg-naf-blue'>".$arrRName[$i]."</h2>";
+
+				for ($j=0; $j < count($arrImg); $j++) {
+
+					if ($arrRID[$j] == $i+1) {
+					 	
+						$output = $output."
+
+						<div class='row my-4'>
+			                <div class='col-md-4 resources-brands text-center'>
+			                    <img class='p-4 my-auto' src='".$arrImg[$j]."' style='width:100%;'>
+			                </div>
+			                
+			                <div class='col-md-8 resources-detail'>
+			                  <h4>".$arrTitle[$j]."</h4>
+			                  <p class='lead'>$arrLead[$j]</p>
+			                  <br>
+			                  <p>".$arrDescription[$j]."</p>
+			                  <a href='".$arrLink[$j]."'>Visit Website</a> 
+			                </div>
+		           		</div>
+
+
+						";
+
+					}	
+				 }
+				
+			}
+
+
+
+				$stmt->close();
+
+		} else {
+
+			$output = "Query to retrieve product information failed.";
+
+		}
+
+		$conn->close();
+
+
+
+
+?>
+
 <div class="container">
-
-      <h1 class="my-5 text-center text-naf-blue">Resources</h1>
-
-      <!--Local Resources -->
-
-      <h2 class="resources-heading">Local Resources</h2>
-      <br>
-      <div class="row my-4">
-          <div class="col-md-4 resources-brands text-center">
-              <img src="img/resource-brand-1.png">
-          </div>
-
-          <div class="col-md-8 resources-detail">
-            <h4>Disabled Crime Victims Assistance, Inc.</h4>
-            <p class="lead">Legal Help</p>
-            <br>
-            <p>Disabled Crime Victims Assistance, Inc. is committed to assisting the disabled crime victim and their families and promoting awareness of their specialized needs to those who serve victims and the community.</p>
-            <a href="#">Visit Website</a>
-          </div>
-      </div>
-
-      <div class="row my-4">
-          <div class="col-md-4 resources-brands text-center">
-              <img src="img/resource-brand-1.png">
-          </div>
-
-          <div class="col-md-8 resources-detail">
-            <h4>Disabled Crime Victims Assistance, Inc.</h4>
-            <p class="lead">Legal Help</p>
-            <br>
-            <p>Disabled Crime Victims Assistance, Inc. is committed to assisting the disabled crime victim and their families and promoting awareness of their specialized needs to those who serve victims and the community.</p>
-            <a href="#">Visit Website</a>
-          </div>
-      </div>
+ <h1 class="my-5 text-center text-naf-blue">Resources</h1>
 
 
-      <div class="row my-4">
-          <div class="col-md-4 resources-brands text-center">
-              <img src="img/resource-brand-1.png">
-          </div>
-
-          <div class="col-md-8 resources-detail">
-            <h4>Disabled Crime Victims Assistance, Inc.</h4>
-            <p class="lead">Legal Help</p>
-            <br>
-            <p>Disabled Crime Victims Assistance, Inc. is committed to assisting the disabled crime victim and their families and promoting awareness of their specialized needs to those who serve victims and the community.</p>
-            <a href="#">Visit Website</a>
-          </div>
-      </div>
+		<?php
 
 
-      <!--Local Resources -->
+		
+		 echo $output
 
-      <h2 class="resources-heading">National Resources</h2>
-      <br>
-      <div class="row my-4">
-          <div class="col-md-4 resources-brands text-center">
-              <img src="img/resource-brand-1.png">
-          </div>
-
-          <div class="col-md-8 resources-detail">
-            <h4>Disabled Crime Victims Assistance, Inc.</h4>
-            <p class="lead">Legal Help</p>
-            <br>
-            <p>Disabled Crime Victims Assistance, Inc. is committed to assisting the disabled crime victim and their families and promoting awareness of their specialized needs to those who serve victims and the community.</p>
-            <a href="#">Visit Website</a>
-          </div>
-      </div>
-
-      <div class="row my-4">
-          <div class="col-md-4 resources-brands text-center">
-              <img src="img/resource-brand-1.png">
-          </div>
-
-          <div class="col-md-8 resources-detail">
-            <h4>Disabled Crime Victims Assistance, Inc.</h4>
-            <p class="lead">Legal Help</p>
-            <br>
-            <p>Disabled Crime Victims Assistance, Inc. is committed to assisting the disabled crime victim and their families and promoting awareness of their specialized needs to those who serve victims and the community.</p>
-            <a href="#">Visit Website</a>
-          </div>
-      </div>
-
-
-      <div class="row my-4">
-          <div class="col-md-4 resources-brands text-center">
-              <img src="img/resource-brand-1.png">
-          </div>
-
-          <div class="col-md-8 resources-detail">
-            <h4>Disabled Crime Victims Assistance, Inc.</h4>
-            <p class="lead">Legal Help</p>
-            <br>
-            <p>Disabled Crime Victims Assistance, Inc. is committed to assisting the disabled crime victim and their families and promoting awareness of their specialized needs to those who serve victims and the community.</p>
-            <a href="#">Visit Website</a>
-          </div>
-      </div>
-
-
+		  ?>
 
 </div>
+
 <?php
   print($htmlFooter);
 ?>
+
