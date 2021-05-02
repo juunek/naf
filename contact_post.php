f7acab99fcfd08aa383725a945893ec8cd5934b1
kathrynkerr <57072945+kathrynkerr@users.noreply.github.com>
2021-05-01 20:30:20 -0500

    made email work

--- contact_post.php
+++ contact_post.php
@@ -4,21 +4,14 @@ print($htmlNav);
 $firstnameC = $_POST['firstname'];
 $lastnameC = $_POST['lastname'];
 $emailC = $_POST['email'];
-$adminemailC = 'kathryn.kerr@mavs.uta.edu';
+$adminemailC = 'kkerr98@sbcglobal.net';
 $phoneC = $_POST['phone'];
 $subjectC = $_POST['subject'];
 $messageC = $_POST['message'];
 
 if (array_key_exists("submit", $_POST)) {
-	// after running through all expected fields, check the $missing array. if there is no required field missing, the $missing array will be empty.
-  $required = array('firstnameC', 'lastnameC', 'emailC', 'subjectC', 'messageC');
-  $expected = array('firstnameC', 'lastnameC', 'emailC', 'phoneC', 'subjectC', 'messageC');
-  $missing = array();
-
-
-if (empty($missing)){
-		if (!empty($emailC)) {
-			$emailC = "<a href=mailto:$emailC>$emailC</a>"; }
+		/*if (!empty($emailC)) {
+			$emailC = "<a href=mailto:$emailC>$emailC</a>"; }*/
 
 			$output="<h1>We've Received Your Message!</h1>";
 
@@ -34,13 +27,8 @@ $mailSent = mail($to,$subject,$message,$header);
 // add $emailResultMessage to the comment preview table as the final output
 $output = $output.$emailResultMessage;
 
-	} else {
-
-				$output = "<h3>Oops!</h3>\n
-								<p>Our system is not able to process your request.  Please see the issues below.</p>\n";
-	}
 } else {
-	$output = "Please use <a href='shoppingcart.php'>this form</a> to make an order.";
+	$output = "<h1>Please use <a href='shoppingcart.php'>this form</a> to make an order.</h1>";
 }
   print($output);
   print($htmlFooter);
