c4134dd75332036ae0d9b6429ea46e59967a0455
tinnguyen-uta <57383385+tinnguyen-uta@users.noreply.github.com>
2021-05-03 14:04:03 -0500

    minor fixed back to top

--- js/bottomRun.js
+++ js/bottomRun.js
@@ -1,20 +1,23 @@
 
-        //Get the button
-    var mybutton = document.getElementById('goTop');
 
-// When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function() {scrollFunction()};
+var mybutton = document.getElementById('goTop');
 
-    function scrollFunction() {
-      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = 'block';
-        } else {
-          mybutton.style.display = 'none';
-        }
-      }
 
-// When the user clicks on the button, scroll to the top of the document
-      function topFunction() {
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 86 || document.documentElement.scrollTop > 86) {
+      document.getElementById("navbar").style.boxShadow = "0 14px 8px -10px grey";
+       mybutton.style.display = 'block';
+  } else {
+      document.getElementById("navbar").style.boxShadow = "0 0px 0px 0px grey";
+      mybutton.style.display = 'none';
+  }
+
+
+}
+
+   function topFunction() {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
       }
--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,4 @@
 
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 86 || document.documentElement.scrollTop > 86) {
-      document.getElementById("navbar").style.boxShadow = "0 14px 8px -10px grey";
-  } else {
-      document.getElementById("navbar").style.boxShadow = "0 0px 0px 0px grey";
-  }
-}
-
-
 
 
 $(document).ready(function() {  
