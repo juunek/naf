105b1b62d24c1de40e4520b280651ab7fe5495f2
tinnguyen-uta <57383385+tinnguyen-uta@users.noreply.github.com>
2021-05-09 01:39:06 -0500

    pagination done for donation and contact pages

--- donationShow.php
+++ donationShow.php
@@ -20,76 +20,112 @@ print("<h2 class='text-center my-5 text-naf-blue'>Manage Donations</h2>");
 function confirmDel(FirstName, KEYID) {
 // javascript function to ask for deletion confirmation
 
-	url = "donationDelete.php?KEYID="+KEYID;
-	var agree = confirm("Delete this item: " + FirstName + " ? ");
-	if (agree) {
-		// redirect to the deletion script
-		location.href = url;
-	}
-	else {
-		// do nothing
-		return;
-	}
+    url = "donationDelete.php?KEYID="+KEYID;
+    var agree = confirm("Delete this item: " + FirstName + " ? ");
+    if (agree) {
+        // redirect to the deletion script
+        location.href = url;
+    }
+    else {
+        // do nothing
+        return;
+    }
 }
 </script>
 
 
 <?php
 
+if (isset($_GET['pageno'])) {
+            $pageno = $_GET['pageno'];
+        } else {
+            $pageno = 1;
+        }
+        $no_of_records_per_page = 10;
+        $offset = ($pageno-1) * $no_of_records_per_page;
 
-$sql = "SELECT KEYID, DTimeStamp, FirstName, LastName, Email, PhoneNumber, DonationType, DonationDetail FROM NAFDonation ORDER BY DTimeStamp DESC, LastName ASC";
+$total_pages_sql = "SELECT COUNT(*) FROM NAFDonation";
+        $result = mysqli_query($conn,$total_pages_sql);
+        $total_rows = mysqli_fetch_array($result)[0];
+        $total_pages = ceil($total_rows / $no_of_records_per_page);
 
-$stmt = $conn->stmt_init();
 
 
-if ($stmt->prepare($sql)) {
-
-            /* execute statement */
-            $stmt->execute();
-
-            /* bind result variables */
-            $stmt->bind_result($KEYID, $DTimeStamp, $FirstName,$LastName, $Email, $PhoneNumber, $DonationType, $DonationDetail);
-
-            $tblRows = "<tbody>";
-
-            while ($stmt->fetch()) {
-							$date=date('m/d/Y', strtotime($DTimeStamp));
-							$fullName = "$FirstName $LastName";
-							$deleteInfo = "$date - $fullName";
-              $Delete_js = htmlspecialchars($deleteInfo, ENT_QUOTES);
-
-                $tblRows = $tblRows."<tr>
-                <td>$date</td><td>$fullName</td><td>$Email</td><td>$PhoneNumber</td><td>$DonationType</td><td>$DonationDetail</td><td><a href='donationForm.php?KEYID=$KEYID'>Edit</a> | <a href='javascript:confirmDel(\"$Delete_js\",$KEYID)'>Delete</a></td></tr></tr>";
-
-            }
-
-            $output = "
-            <table class='table'>\n
-            <thead class='table-head'>\n
-            <tr><th scope='col'>Submission Date</th><th scope='col'>Full Name</th><th scope='col'>Email</th><th scope='col'>Phone Number</th><th scope='col'>Donation Type</th><th scope = 'col'>Donation Detail</th><th scope = 'col'>Options</th></tr>\n
-            <thead>\n".$tblRows.
-            "</tbody>\n</table>\n";
-
-            $stmt->close();
-
-    } else {
-            $output = "Query to retrieve product information failed.";
-    }
-
-    $conn->close();
+    $sql = "SELECT KEYID, DTimeStamp, FirstName, LastName, Email, PhoneNumber, DonationType, DonationDetail FROM NAFDonation ORDER BY DTimeStamp DESC, LastName ASC LIMIT $offset, $no_of_records_per_page";
+      
+ $res_data = mysqli_query($conn,$sql);
 
 ?>
+  
+<div class="container-fluid">
+    <div class='flexboxContainer'>
+      <div><a href="donationForm.php"><span class='button'> + </span> Add a new item</a></div>
+      <div>
+        <table class='table'>
+            <thead class='table-head'>
+                <tr><th scope='col'>Submission Date</th><th scope='col'>Full Name</th><th scope='col'>Email</th><th scope='col'>Phone Number</th><th scope='col'>Donation Type</th><th scope = 'col'>Donation Detail</th><th scope = 'col'>Options</th><th scope = 'col'>Function</th></tr>
+            </thead>
+            <tbody>
 
-                 <!-- Content start here-->
-                <div class="container-fluid">
-                <div class='flexboxContainer'>
-                  <div><a href="donationForm.php"><span class='button'> + </span> Add a new item</a></div>
-                        <div>
+    <?php
+           while($row = mysqli_fetch_array($res_data)){
+                            $date=date('m/d/Y', strtotime($DTimeStamp));
+                            $fullName = "$FirstName $LastName";
+                            $deleteInfo = "$date - $fullName";
+              $Delete_js = htmlspecialchars($deleteInfo, ENT_QUOTES);
 
-                            <?php echo $output ?>
-                        </div>
-                    </div>
-                </div>
+    ?>
+
+
+
+            <tr>
+                <td><?php echo $row["DTimeStamp"]; ?></td>
+                <td><?php echo $row["FirstName"]; ?></td>
+                <td><?php echo $row["LastName"]; ?></td>
+                <td><?php echo $row["Email"]; ?></td>
+                <td><?php echo $row["PhoneNumber"]; ?></td>
+                <td><?php echo $row["DonationType"]; ?></td>
+                <td><?php echo $row["DonationDetail"]; ?></td>
+           
+                   <?php
+
+                $KEYID = $row['KEYID'];
+                
+                 echo "<td><a href='donationForm.php?KEYID=$KEYID'>Edit</a> | <a href='javascript:confirmDel(\"$Delete_js\",$KEYID)'>Delete</a></td>" 
+                 ?>
+            </tr>
+
+    <?php
+    };
+    ?>
+    </tbody>
+    </table>
+
+    <?php
+    mysqli_close($conn);
+
+    ?>
+    </div>
+    </div>
+     <div class="d-flex justify-content-center">
+        <ul class="pagination">
+            <li class="page-item"><a href="?pageno=1" class="page-link">First  </a></li>
+            <li class="page-item <?php if($pageno <= 1){ echo 'disabled'; } ?>">
+                <a href="<?php if($pageno <= 1){ echo '#'; } else { echo "?pageno=".($pageno - 1); } ?>" class="page-link">Prev  </a>
+            </li>
+            <li class="page-item <?php if($pageno >= $total_pages){ echo 'disabled'; } ?>">
+                <a href="<?php if($pageno >= $total_pages){ echo '#'; } else { echo "?pageno=".($pageno + 1); } ?>" class="page-link">Next  </a>
+            </li>
+            <li page-item ><a href="?pageno=<?php echo $total_pages; ?>" class="page-link">Last</a></li>
+        </ul>
+    </div>
+
+
+           
+         
+             
+     
+     </div>
 
 
 <?php
