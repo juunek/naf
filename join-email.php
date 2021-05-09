f37e3c3aaad4069a84f09ce6e7752b1c6e7cd8de
katelyn-raye <57073079+katelyn-raye@users.noreply.github.com>
2021-05-08 19:18:04 -0500

    added page title and meta description

--- join-email.php
+++ join-email.php
@@ -3,10 +3,11 @@
 <!doctype html>
 <html lang="en">
   <head>
-  <title>Neuro Assitance Foundation</title>
+  <title>Join Our Newsletter | Neuro Assitance Foundation</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <meta name="description" content="Stay updated with all of Neuro Assistance Foundation's latest non-profit news and events.">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
